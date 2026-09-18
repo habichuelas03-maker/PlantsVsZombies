e18d52b110f3bc15af65c0f9f82d83986a05db5c
rvp <vrebeca808@gmail.com>
2026-09-18 17:43:11 -0600

    se optimizo escenario Game

---
+++ Assets/Scripts/Lane.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class Lane : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] zones;
+    public Transform[] Zones => zones;
+    [SerializeField]
+    private Transform laneStart;
+    public Transform LaneStart => laneStart;
+}
