3340719459f8fcb6ee8317d62fd66c74b04735de
habichuelas03-maker <habichuelas03@gmail.com>
2026-09-21 14:33:56 -0600

    se agrego script zombie data

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Slider healthSlider;
+    [SerializeField] private UnityEvent onDeath;
+    [SerializeField] private UnityEvent onTakeDamage;
+    private float currentHealth;
+    private float maxHealth;
+
+    public bool IsDead => currentHealth <= 0;
+
+    public void SetMaxHealth(float health)
     {
-        
+        maxHealth = health;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Initialize()
     {
-        
+        currentHealth = maxHealth;
+        UpdateHealthSlider();
+    }
+
+    private void UpdateHealthSlider()
+    {
+        healthSlider.value = currentHealth / maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        onTakeDamage?.Invoke();
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            onDeath?.Invoke();
+        }
+
+        UpdateHealthSlider();
     }
 }
