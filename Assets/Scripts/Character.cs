42363357cbf8782b7916462fe7884cb22caec22a
DiegoSF9009 <soldadodecolorrosa@gmail.com>
2026-09-22 20:26:33 -0600

    Cuarta clase_se agrego plant, animations, detect target y se modifico character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
-
+using System.Collections; 
+ 
 public class Character : MonoBehaviour
 {
     protected Health health;
     protected Collider characterCollider;
+    [SerializeField]
     protected Animator characterAnimator;
-
     protected virtual void Awake()
     {
         health = GetComponent<Health>();
         characterCollider = GetComponent<Collider>();
-        characterAnimator = GetComponent<Animator>();
+    }
+    public virtual void Die()
+    {
+        characterCollider.enabled = false;
+        StartCoroutine(DieCoroutine());
+    }
+    private IEnumerator DieCoroutine()
+    {
+        characterAnimator.Play("Die", 0, 0f);
+        yield return characterAnimator.WaitForCurrentAnimation();
+        gameObject.SetActive(false);
     }
 }
+ 
